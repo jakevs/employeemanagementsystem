bdba81a726a578f4cfde3ae94e4a1781e7b1c491
James Van Slyke <jakevs3@gmail.com>
2020-06-10 00:36:40 -0500

    added switch case

--- main.js
+++ main.js
@@ -12,17 +12,32 @@ async function init() {
 }
 function questions() {
   const prompts = false;
-  inquirer.prompt({
-    type: "list",
-    name: "actions",
-    message: "What would you like to select first?",
-    choices: [
-      "View All Employees",
-      "View All Roles",
-      "View All Departments",
-      "Goodbye"
-    ]
-  });
+  inquirer
+    .prompt({
+      type: "list",
+      name: "actions",
+      message: "What would you like to select first?",
+      choices: [
+        "View All Employees",
+        "View All Roles",
+        "View All Departments",
+        "Goodbye"
+      ]
+    })
+    .then(function (selection) {
+      switch (selection.actions) {
+        case "View All Employees":
+          break;
+        case "View All Roles":
+          break;
+        case "View All Departments":
+          break;
+        case "Goodbye":
+          break;
+        default:
+          break;
+      }
+    });
   // const { again, ...answers } = await inquirer.prompt(prompts);
   // const newInputs = [...inputs, answers];
   // return again ? questions(newInputs) : newInputs;
