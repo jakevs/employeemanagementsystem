d7a39d52beeee4b798746ef9a8218bbb0680f8f3
jakevs <jakevs3@gmail.com>
2020-06-21 00:02:09 -0500

    fixing seed issues

--- db/seed.sql
+++ db/seed.sql
@@ -2,12 +2,26 @@ USE employees;
 
   
 INSERT INTO department (department_name)
-VALUES ("My House"),("Office Heads");
+VALUES 
+("My House"),
+("Office Heads");
 
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Deputy Director", 71000.00, 1),("Director", 90000.00, 2),("Singer", 35000.00, 1),("Trouble Maker", 40000.00, 2);
+VALUES 
+("Deputy Director", 71000.00, 1),
+("Director", 90000.00, 2),
+("Singer", 35000.00, 1),
+("Trouble Maker", 40000.00, 2);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("LeVALUES ("Molly", "Van Slyke", 1, 1),("James", "Van Slyke", 2, NULL),("Maddy", "Van Slyke", 3, NULL),("Grayson", "Van Slyke", 4, NULL);
+VALUES
+    ('James', 'Van Slyke', 1, NULL),
+    ('Molly', 'Van Slyke', 2, 1),
+    ('Madeline', 'Van Slyke', 3, NULL),
+    ('Grayson', 'Van Slyke', 4, 3),
+    ('Jim', 'Van Slyke', 5, NULL),
+    ('Patti', 'Van Slyke', 6, 5),
+    ('Scott', 'Shreders', 7, NULL),
+    ('Matt', 'Geier', 8, 7);
